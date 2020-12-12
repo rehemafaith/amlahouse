38de42067a02764c6d41d067defee7ecffb40632
rehemafaith <rehemafaith01@gmail.com>
2020-12-12 09:59:23 -0500

    first template design.incomplete

---
+++ main.js
@@ -0,0 +1,8 @@
+function disclaimer(){
+    var disclaim =document.getElementById('disclaimer');
+    if (disclaim.style.display === "block") {
+        disclaim.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+}
